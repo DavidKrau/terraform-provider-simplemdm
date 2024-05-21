b2e99c882f73a95b81f443b99aa0b19bb698c99f
David Kraushuber <david.kraushuber@mytaxi.com>
2024-05-21 11:21:35 +0200

    script data source

---
+++ provider/script_data_source_test.go
@@ -0,0 +1,23 @@
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccScriptDataSource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Read testing
+			{
+				Config: providerConfig + `data "simplemdm_script" "test" {id ="5727"}`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify name of the app
+					resource.TestCheckResourceAttr("data.simplemdm_script.test", "name", "Test script"),
+				),
+			},
+		},
+	})
+}
