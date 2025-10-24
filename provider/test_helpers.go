1071042e8cf6ce4749bc7f2e5678329a085eabe8
Brian <1037563+samspade21@users.noreply.github.com>
2025-10-24 00:18:15 -0700

    Skip acceptance tests when TF_ACC is unset (#34)

--- provider/test_helpers.go
+++ provider/test_helpers.go
@@ -8,6 +8,10 @@ import (
 // testAccPreCheck ensures acceptance tests run only when TF_ACC is enabled
 // and the required authentication information is present.
 func testAccPreCheck(t *testing.T) {
+	if os.Getenv("TF_ACC") == "" {
+		t.Skip("Acceptance tests require TF_ACC to be set")
+	}
+
 	if os.Getenv("SIMPLEMDM_APIKEY") == "" {
 		t.Skip("Acceptance tests require SIMPLEMDM_APIKEY to be set")
 	}
